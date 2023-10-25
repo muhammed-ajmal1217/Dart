f087966be07a4623b0e08e1a1368a1f5e53d5dc4
muhammed-ajmal1217 <stranger99980@gmail.com>
2023-10-25 14:51:55 +0530

    switch case

--- switch_case.dart
+++ switch_case.dart
@@ -1,3 +1,43 @@
 void main(List<String> args) {
-  
+  int marks = 76;
+  String grade = getGrade(marks);
+
+  switch (grade) {
+    case "A+":
+      print("Excellent! You got an A+.");
+      break;
+    case "A":
+      print("Great job! You got an A.");
+      break;
+    case "B":
+      print("Good work! You got a B.");
+      break;
+    case "C":
+      print("You got a C. Keep improving.");
+      break;
+    case "D":
+      print("You got a D. Study harder next time.");
+      break;
+    case "F":
+      print("Sorry, you failed. Please retake the course.");
+      break;
+    default:
+      print("Invalid grade.");
+  }
+}
+
+String getGrade(int marks) {
+  if (marks >= 90 && marks <= 100) {
+    return "A+";
+  } else if (marks >= 80 && marks < 90) {
+    return "A";
+  } else if (marks >= 70 && marks < 80) {
+    return "B";
+  } else if (marks >= 60 && marks < 70) {
+    return "C";
+  } else if (marks >= 50 && marks < 60) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
